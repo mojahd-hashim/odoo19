6ad3429143ae6591954466f5e7c6e7d57500b64b
odoo-kab <dev.mojahid11@gmail.com>
2026-06-03 14:20:52 +0300

    update

--- waqf_portal_access/models/portal_user.py
+++ waqf_portal_access/models/portal_user.py
@@ -157,7 +157,7 @@ class WaqfPortalUser(models.Model):
         # ② أزل مجموعة المستخدم الداخلي
         internal_group = self.env.ref('base.group_user')
         internal_group.sudo().write({
-            'users': [(3, user.id)]  # 3 = أزل
+            'user_ids': [(3, user.id)]  # 3 = أزل
         })
         # ② إضافته لمجموعة البوابة
         portal_group = self.env.ref('base.group_portal')
